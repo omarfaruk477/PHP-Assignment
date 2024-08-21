9190d3b0bd48439247304a9e23d136601abd3d4e
omarfaruk477 <omarfaruk477@gmail.com>
2024-08-21 11:27:02 +0600

    first assignment updated

---
+++ edgecu/app/data.php
@@ -0,0 +1,64 @@
+<?php
+
+$sliders = [
+    [
+        "title"     => "We love PHP",
+        "image"     => "./images/1.jpg",
+        "desc"      => "PHP is our dream",
+        "btn1"      =>  [
+                        "title"     => "Button 1",
+                        "link"      => "#",
+                        "bg_color"  => "#4B088A"
+                        ],
+         "btn2"      =>  [
+                        "title"     => "Button 2",
+                        "link"      => "#",
+                         "bg_color"  => "#6A0888"
+                        ]
+    ],
+    [
+            "title"     => "We love ARRAY",
+            "image"     => "./images/2.jpg",
+            "desc"      => "PHP is our dream",
+            "btn1"      =>  [
+                            "title"     => "Button 1",
+                            "link"      => "#",
+                            "bg_color"  => "#008000"
+                            ],
+             "btn2"      =>  [
+                            "title"     => "Button 2",
+                            "link"      => "#",
+                             "bg_color"  => "#00e600"
+                            ]
+    ], 
+    [
+                "title"     => "We love Laravel",
+                "image"     => "./images/3.jpg",
+                "desc"      => "PHP is our dream",
+                "btn1"      =>  [
+                                "title"     => "Button 1",
+                                "link"      => "#",
+                                "bg_color"  => "#b32400"
+                                ],
+                 "btn2"      =>  [
+                                "title"     => "Button 2",
+                                "link"      => "#",
+                                 "bg_color"  => "#991f00"
+                                ]
+    ]
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
